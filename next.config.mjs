e896dd41474ffa3c2374a40b835b2be7435e44b2
PareshNakum <pareshnakum517@gmail.com>
2025-08-05 12:14:16 +0530

    dfsdf

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,18 @@ const nextConfig = {
   // reactStrictMode: true,
   // output: 'export',
   // trailingSlash: true,
+  headers: [
+    {
+      key: "Cache-Control",
+      value: "public, max-age=0, must-revalidate",
+    },
+  ],
   images: {
     unoptimized: true,
-    domains: ['rpdiamondsandjewellery-staging.s3.ap-southeast-1.amazonaws.com','rpdiamondsandjewellery-uat.s3.ap-southeast-1.amazonaws.com'],
+    domains: [
+      "rpdiamondsandjewellery-staging.s3.ap-southeast-1.amazonaws.com",
+      "rpdiamondsandjewellery-uat.s3.ap-southeast-1.amazonaws.com",
+    ],
   },
 };
 
