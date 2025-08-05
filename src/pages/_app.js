361b6d8ab0a4992855d5be354db4d98bbb31463e
PareshNakum <pareshnakum517@gmail.com>
2025-08-05 15:06:17 +0530

    asdasd

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -36,6 +36,40 @@ const Header = dynamic(
   }
 );
 
+export async function getServerSideProps() {
+  const origin = "https://zurah1.vercel.app/";
+
+  const response = await fetch("https://apiuat-ecom.upqor.com/call/EmbeddedPageMaster", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      origin,
+      prefer: origin,
+    },
+    body: JSON.stringify({
+      a: "GetStoreData",
+      store_domain: origin,
+      SITDeveloper: "1",
+    }),
+  });
+
+  const result = await response.json();
+  const storeEntityIds = result?.success === 1 ? result?.data : {};
+
+  return {
+    props: {
+      storeEntityIds,
+      seoData: {
+        title: storeEntityIds?.seo_titles || "Zurah Jewellery",
+        description: storeEntityIds?.seo_description || "Elegant jewellery for all occasions",
+        keywords: storeEntityIds?.seo_keyword || "Zurah, Jewellery",
+        image: storeEntityIds?.preview_image,
+        url: origin,
+      },
+    },
+  };
+}
+
 function InnerApp({ Component, pageProps }) {
   console.log(pageProps);
   const dispatch = useDispatch();
