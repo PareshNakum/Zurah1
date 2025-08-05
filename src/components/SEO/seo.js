65a15193d17ee7f9861c2730f8b3e0f041ee80a4
PareshNakum <pareshnakum517@gmail.com>
2025-08-05 17:18:44 +0530

    sadasd

--- src/components/SEO/seo.js
+++ src/components/SEO/seo.js
@@ -11,7 +11,7 @@ export default function Seo({ title, description, keywords, image, url }) {
       {/* Open Graph */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      {/* <meta property="og:image" content={image} /> */}
       <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
 
@@ -19,7 +19,7 @@ export default function Seo({ title, description, keywords, image, url }) {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      {/* <meta name="twitter:image" content={image} /> */}
     </Head>
   );
 }
