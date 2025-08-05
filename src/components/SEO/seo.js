221d5188b537bd2e1684068f8b4d119b740402f7
PareshNakum <pareshnakum517@gmail.com>
2025-08-05 13:45:05 +0530

    sdsdasd

--- src/components/SEO/seo.js
+++ src/components/SEO/seo.js
@@ -6,10 +6,15 @@ export default function Seo({ title, description, keywords, image, url }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
+
+      {/* Open Graph */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
       <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter Card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
